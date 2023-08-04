4376bedba4afbb83c63fadfb6864838328335a07
Iamneo <techadmin@iamneo.ai>
2023-08-04 20:31:52 +0000

    commit

---
+++ springapp/src/main/test copy.js
@@ -0,0 +1,70 @@
+console.log("APP...")
+
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasg")
+}
+
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasg")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasg")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasg")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasg")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasg")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasg")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasg")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasg")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasg")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasg")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasg")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasg")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasg")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasg")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasg")
+}
+
+for(let i = 0; i <= 100; i++) {
+    console.log("fdasg")
+}
